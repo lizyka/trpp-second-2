2dd99cd28373383d3dbe0d2096e312dccb36e9b8
Liza <vorrrl@mail.ru>
2021-04-13 10:45:21 +0300

    Too long string

--- src/main/java/ru/mirea/trpp/controllers/StudentController.java
+++ src/main/java/ru/mirea/trpp/controllers/StudentController.java
@@ -1,10 +1,10 @@
-package ru.mirea.trpp_second_2.controllers;
+package ru.mirea.trpp.controllers;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import ru.mirea.trpp_second_2.entity.Student;
+import ru.mirea.trpp.entity.Student;
 
 import java.io.InputStreamReader;
 import java.util.List;
@@ -19,7 +19,8 @@ public class StudentController {
 
     /** Конструктор. */
     public StudentController() {
-        studentList = new CsvToBeanBuilder<Student>(new InputStreamReader(this.getClass().getResourceAsStream("/MOCK_DATA.csv"))).withType(Student.class).build().parse();
+        studentList = new CsvToBeanBuilder<Student>(new InputStreamReader(this.getClass().
+                getResourceAsStream("/MOCK_DATA.csv"))).withType(Student.class).build().parse();
     }
 
     /**
